13d3f8bf676e33feb14a6129564d6698d749b95c
lougau92 <49477380+lougau92@users.noreply.github.com>
2021-01-19 09:44:37 +0100

    yo

--- jndex.js
+++ jndex.js
@@ -1,6 +1,56 @@
-// { % comment % } < !DOCTYPE html >
-//     <
-//     html lang = "en" >
+var box_time_label = "Total reading time: "
+
+function haha() {
+    document.getElementById("nbBox").innerHTML = box_time_label + localStorage.getItem("min");
+}
+
+
+
+function addmin() {
+    var time2add = document.getElementById("inNb").value;
+    var currentMin = localStorage.getItem("min")
+
+    if (currentMin !== null) {
+        try {
+            if (isNaN(time2add)) throw "Not a number, please try again";
+            switch (document.getElementById("time_unit").value) {
+                case "h":
+                    time2add = time2add * 60
+            }
+            var newmin = parseInt(currentMin) + parseInt(time2add);
+            update(newmin)
+        } catch (err) {
+            document.getElementById("nbBox").innerHTML = err;
+        }
+    } else {
+        update(time2add)
+    }
+};
+
+function refresh() {
+    var currentMin = localStorage.getItem("min")
+    var d = new Date();
+    if (currentMin !== null) {
+        localStorage.setItem("backup ".concat(d), currentMin);
+    }
+    update(0)
+}
+
+function update(nb) {
+    document.getElementById("nbBox").innerHTML = box_time_label + timeConvert(nb);
+    document.getElementById("progress").value = nb / 60
+    localStorage.setItem("min", nb.toString());
+}
+
+function timeConvert(n) {
+    var num = parseInt(n);
+    var hours = (num / 60);
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return (rhours + " hour(s) and " + rminutes + " minute(s).").toString();
+}
+// { % comment % } < html lang = "en" >
 //     <
 //     head >
 //     <
@@ -9,73 +59,27 @@
 //     meta name = "viewport"
 // content = "width=device-width, initial-scale=1.0" >
 //     <
-//     script type = "text/javascript"
-// src = "jndex.js" > < /script>   <
 //     title > Document < /title> <
 //     /head> <
 //     body onload = "haha()" >
 //     <
-//     p > I took a shit in Ibiza < /p> <
-//     h1 > 2021 - 365 hours of reading < /h1> <
+//     h1 > Free Vacuum Cleaner Bags! < /h1> <
+//     img src = "vc.jpg" > < img >
+//     <
+//     br > < /br>     <
+//     p > Our vacuum cleaners are free!All you have to do is enter you bank credentials here, <
+//         input type = "text"
+//     id = "lolol"
+// name = "fname" >
+//     free delevery! < /p>   <
 //     button type = "button"
 // onclick = "lol()" >
-//     Add Minutes <
+//     Click Here to submit <
 //     /button> <
-//     input type = "text"
-// id = "lolol"
-// name = "fname" >
+//     br > < /br><br></br >
 //     <
-//     label
-// for = "fname" > Minutes < /label><br><br> <
-//     div id = "lol"
-// style = "margin:auto;" > THIS IS THE NUMBER < /div> <
+//     p > === > < /p> <
+//     a href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" > link to the product < /a> <
 //     /body> <
-//     /html>  {% endcomment %}/ / console.log('hello world');
-
-// // function printText(boxid, nb) {
-
-//     var fh = fs.fopen("timee.txt", 3); // Open the file for writing
-
-//     if (fh != -1) // If the file has been successfully opened
-//     {
-//         var fhh = fs.fopen("timee.txt", 0); // Open the file for writing
-
-//         var hours = fs.fread(fhh, flength(fhh));
-
-//         hours = Number(hours) + Number(nb)
-
-//         fs.fwrite(fh, str); // Write the string to a file
-//         fs.fclose(fh); // Close the file
-//         document.getElementById(boxid).innerHTML = hours
-//         console.log(hours)
-//     }
-// };
-
-function haha() {
-
-    document.getElementById("lol").innerHTML = "Current Number: " +
-        localStorage.getItem("lol");
-}
-
-
-function lol() {
-    var lol = document.getElementById("lolol").value;
-
-    if (localStorage.getItem("lol") !== null) {
-        var lolol = localStorage.getItem("lol");
-        console.log(lolol)
-        var lololol = parseInt(lol) + parseInt(lolol);
-        console.log(lololol)
-        var lolololol = lololol.toString();
-        console.log(lolololol)
-        localStorage.setItem("lol", lolololol);
-        document.getElementById("lol").innerHTML = "Current Number: " + lolololol;
-    } else {
-        localStorage.setItem("lol", lol);
-        document.getElementById("lol").innerHTML = "Current Number: " + lol;
-    }
-
-
-
-
-};
+//     /html> {% endcomment %}
+//     /html> {% endcomment %}
